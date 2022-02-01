ca239a0db0d5ff9f5c98186e8f0b57db2406c97a
Samuel Corsi-House <chouse.samuel@gmail.com>
2022-01-31 19:15:10 -0500

    chore(lib): reset lib, starting a new one like sapphire

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
 export { version } from '../package.json';
-
-// export * from './struct/SnowClient';
-// export * from './struct/SnowModule';
-
-// export * from './utils/Category';
-
-// export * from './typings';
